9e636ed778730e003b1ae057191e9ea980527ec4
James Chen <jianch@microsoft.com>
2021-09-26 15:17:55 -0700

    remove all password

--- cargradient_api/settings.py
+++ cargradient_api/settings.py
@@ -16,12 +16,13 @@ import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cdqfy0f^4$*xh$q$7pm9r@6r0ng0icp5^%6ve&j%rueh-)elgf'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +85,10 @@ WSGI_APPLICATION = 'cargradient_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'carstudio_db',
-        'USER': 'carstudio_admin@carstudio-data',
-        'PASSWORD': '!mysearchengine1',
-        'HOST': 'carstudio-data.postgres.database.azure.com',
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': '5432',
     }
 }
@@ -129,9 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
